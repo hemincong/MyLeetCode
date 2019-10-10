f0dbff05c6bd31d1a388b0bede7e09148688cce2
Manchung.Ho <hemincong@gmail.com>
2019-10-11 00:31:25 +0800

    + 452. Minimum Number of Arrows to Burst Balloons

---
+++ 452_Minimum_Number_of_Arrows_to_Burst_Balloon.cpp
@@ -0,0 +1,62 @@
+//
+// Created by mincong.he on 2019/10/10.
+//
+#include <vector>
+
+using namespace std;
+
+bool compare(const std::vector<int> &a, const std::vector<int> &b) {
+    return a.at(0) < b.at(0);
+}
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>> &points) {
+        if (points.empty()) {
+            return 0;
+        }
+
+        std::sort(points.begin(), points.end(), compare);
+
+        int shoot_num = 1;
+        int shoot_begin = points.at(0).at(0);
+        int shoot_end = points.at(0).at(1);
+
+        for (int i = 1; i < points.size(); i++) {
+            auto current_point = points.at(i);
+            auto current_point_begin = current_point.at(0);
+            auto current_point_end = current_point.at(1);
+            if (current_point_begin >= shoot_begin && current_point_begin <= shoot_end) {
+                // 已经排过序
+                shoot_begin = current_point_begin;
+                if (current_point_end < shoot_end) {
+                    shoot_end = current_point_end;
+                }
+            } else {
+                shoot_num++;
+                shoot_begin = current_point_begin;
+                shoot_end = current_point_end;
+            }
+        }
+
+        return shoot_num;
+    }
+};
+
+int main(int argc, char **argv) {
+    Solution s;
+    std::vector<std::vector<int> > points;
+    points.emplace_back((std::vector<int>({10, 16})));
+    points.emplace_back((std::vector<int>({2, 8})));
+    points.emplace_back((std::vector<int>({1, 6})));
+    points.emplace_back((std::vector<int>({7, 12})));
+    printf("%d\n", s.findMinArrowShots(points));
+
+    std::vector<std::vector<int> > points_2;
+    points_2.emplace_back((std::vector<int>({1, 2})));
+    points_2.emplace_back((std::vector<int>({2, 3})));
+    points_2.emplace_back((std::vector<int>({3, 4})));
+    points_2.emplace_back((std::vector<int>({4, 5})));
+    printf("%d\n", s.findMinArrowShots(points_2));
+    return EXIT_SUCCESS;
+}
