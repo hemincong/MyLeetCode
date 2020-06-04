6d4d6e7e8506486f0c213cfa47cec23c8bb6e2ab
hemc <hemc@szca.com>
2020-06-05 01:50:49 +0800

    + 3. Longest Substring Without Repeating Characters

---
+++ 3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,54 @@
+//
+// Created by mincong.he on 2020/6/3.
+//
+#include <map>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty()) return 0;
+        if (s.size() == 1) return 1;
+
+        int left_index = 0;
+        int right_index = 0;
+        int longest = 0;
+        map<char, size_t> curr_char_count;
+
+        while (right_index < s.size()) {
+            char r_char;
+            do {
+                r_char = s[right_index];
+                curr_char_count[r_char]++;
+                if (right_index - left_index > longest && curr_char_count[r_char] == 1) {
+                    longest = right_index - left_index;
+                }
+                right_index++;
+            } while (right_index < s.size() && curr_char_count[r_char] < 1);
+            while (curr_char_count[r_char] > 1 && left_index < right_index) {
+                auto l_char = s[left_index];
+                curr_char_count[l_char]--;
+                left_index++;
+            }
+        }
+        return longest + 1;
+    }
+};
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(TestRand, rand_test_1) {
+    Solution solution;
+    EXPECT_EQ(solution.lengthOfLongestSubstring("bbbbb"), 1);
+    EXPECT_EQ(solution.lengthOfLongestSubstring("abcabcbb"), 3);
+    EXPECT_EQ(solution.lengthOfLongestSubstring("dvdf"), 3);
+    EXPECT_EQ(solution.lengthOfLongestSubstring(" "), 1);
+    EXPECT_EQ(solution.lengthOfLongestSubstring(""), 0);
+    EXPECT_EQ(solution.lengthOfLongestSubstring("au"), 2);
+    EXPECT_EQ(solution.lengthOfLongestSubstring("pwwkew"), 3);
+}
