ff80b0f7caecc5d9ae8e8d00328ed02ad13819d9
hemc <hemc@szca.com>
2020-12-15 09:58:24 +0800

    55 more solution

--- 55_Jump_Game.cpp
+++ 55_Jump_Game.cpp
@@ -4,6 +4,8 @@
 
 #include <vector>
 
+#include "AlgoUtils.h"
+
 using namespace std;
 
 class Solution {
@@ -29,13 +31,38 @@ public:
     }
 };
 
-int main(int argc, char **argv) {
-    Solution solve;
-    std::vector<int> nums_1 = {2, 3, 1, 1, 4};
-    assert(solve.canJump(nums_1));
+class Solution_2 {
+public:
+    bool canJump(vector<int> &nums) {
+        if (nums.size() == 1 && nums.at(0) == 0) return true;
 
+        int longest = nums.at(0);
+        for (int i = 0; i < nums.size(); ++i) {
+            if (longest < i) break;
+            auto curr_can_jump = i + nums.at(i);
+            longest = curr_can_jump > longest ? curr_can_jump : longest;
+        }
+        return longest >= nums.size() - 1;
+    }
+};
+
+int main(int argc, char **argv) {
+    std::vector<int> nums_0 = {0};
+    std::vector<int> nums_1 = {2, 3, 1, 1, 4};
     std::vector<int> nums_2 = {3, 2, 1, 0, 4};
-    assert(!solve.canJump(nums_2));
+    std::vector<int> nums_3 = {2, 0, 0};
+
+    Solution solve;
+    EXPECT_EQ(solve.canJump(nums_0), true);
+    EXPECT_EQ(solve.canJump(nums_1), true);
+    EXPECT_EQ(solve.canJump(nums_2), false);
+    EXPECT_EQ(solve.canJump(nums_3), true);
+
+    Solution_2 solve_2;
+    EXPECT_EQ(solve_2.canJump(nums_0), true);
+    EXPECT_EQ(solve_2.canJump(nums_1), true);
+    EXPECT_EQ(solve_2.canJump(nums_2), false);
+    EXPECT_EQ(solve_2.canJump(nums_3), true);
 
     return EXIT_SUCCESS;
 }
