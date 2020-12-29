8b16e9d1542f51857a3a696f70864d183f819305
hemc <hemc@szca.com>
2020-12-29 17:54:35 +0800

    + 233. Number of Digit One

---
+++ 233_Number_of_Digit_One.cpp
@@ -0,0 +1,41 @@
+//
+// Created by mincong.he on 2020/12/29.
+//
+#include "AlgoUtils.h"
+
+#include <string>
+
+class Solution {
+public:
+    int countDigitOne(int n) {
+        auto num_str = std::to_string(n);
+        auto num_size = num_str.size();
+
+        size_t count = 0;
+        for (int i = 1; i <= num_size; i++) {
+            int a = n / size_t(pow(10, i));
+            int b = n % size_t(pow(10, i - 1));
+            count += (size_t(pow(10, i - 1)) * a);
+
+            auto digit = num_str[num_size - i] - '0';
+            if (digit > 1) {
+                count += size_t(pow(10, i - 1));
+            } else if (digit == 1) {
+                count += b;
+                count += 1;
+            }
+        }
+        return count;
+    }
+};
+
+int main(int argc, char **argv) {
+    Solution s;
+    EXPECT_EQ(s.countDigitOne(0), 0);
+    EXPECT_EQ(s.countDigitOne(13), 6);
+    EXPECT_EQ(s.countDigitOne(824883294), 767944060);
+    EXPECT_EQ(s.countDigitOne(999999999), 900000000);
+    EXPECT_EQ(s.countDigitOne(1410065408), 1737167499);
+    EXPECT_EQ(s.countDigitOne(1633388154), 2147483646);
+    return EXIT_SUCCESS;
+}
