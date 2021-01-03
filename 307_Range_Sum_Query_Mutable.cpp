e7c7e97a69ac84240ddcdbada09926df04753ba8
hemc <hemc@szca.com>
2021-01-03 18:15:41 +0800

    + 307. Range Sum Query-Mutable

---
+++ 307_Range_Sum_Query_Mutable.cpp
@@ -0,0 +1,96 @@
+//
+// Created by mincong.he on 2021/1/2.
+//
+
+#include "AlgoUtils.h"
+#include <vector>
+
+using namespace std;
+
+void build_segment_tree(std::vector<int> &value,
+                        std::vector<int> &nums,
+                        int pos, int left, int right) {
+    if (left == right) {
+        value[pos] = nums[left];
+        return;
+    }
+    int mid = (left + right) / 2;
+    build_segment_tree(value, nums, pos * 2 + 1, left, mid);
+    build_segment_tree(value, nums, pos * 2 + 2, mid + 1, right);
+    value[pos] = value[pos * 2 + 1] + value[pos * 2 + 2];
+}
+
+int sum_range_segment_tree(std::vector<int> &value,
+                           int pos, int left, int right,
+                           int qleft, int qright) {
+    if (qleft > right || qright < left) {
+        return 0;
+    }
+    if (qleft <= left && qright >= right) {
+        return value[pos];
+    }
+    int mid = (left + right) / 2;
+    return sum_range_segment_tree(value, pos * 2 + 1,
+                                  left, mid, qleft, qright) +
+           sum_range_segment_tree(value, pos * 2 + 2, mid + 1,
+                                  right, qleft, qright);
+}
+
+void update_segment_tree(std::vector<int> &value,
+                         int pos, int left, int right,
+                         int index, int new_value) {
+    if (left == right && left == index) {
+        value[pos] = new_value;
+        return;
+    }
+    int mid = (left + right) / 2;
+    if (index <= mid) {
+        update_segment_tree(value, pos * 2 + 1,
+                            left, mid, index, new_value);
+    } else {
+        update_segment_tree(value, pos * 2 + 2, mid + 1,
+                            right, index, new_value);
+    }
+    value[pos] = value[pos * 2 + 1] + value[pos * 2 + 2];
+}
+
+
+class NumArray {
+public:
+    NumArray(vector<int> &nums) :
+            _value(nums.size() * sizeof(int), 0),
+            _right_end(nums.size() - 1) {
+        if (nums.empty()) return;
+
+        build_segment_tree(_value, nums, 0, 0, _right_end);
+    }
+
+    void update(int i, int val) {
+        update_segment_tree(_value, 0, 0, _right_end, i, val);
+    }
+
+    int sumRange(int i, int j) {
+        return sum_range_segment_tree(_value, 0, 0, _right_end, i, j);
+    }
+
+private:
+    std::vector<int> _value;
+    int _right_end;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(i,val);
+ * int param_2 = obj->sumRange(i,j);
+ */
+
+int main(int argc, char **argv) {
+    vector<int> test_case_1{1, 3, 5};
+    NumArray numArray_1(test_case_1);
+    EXPECT_EQ(numArray_1.sumRange(0, 2), 9);
+    numArray_1.update(1, 2);
+    EXPECT_EQ(numArray_1.sumRange(0, 2), 8);
+
+    return EXIT_SUCCESS;
+}
