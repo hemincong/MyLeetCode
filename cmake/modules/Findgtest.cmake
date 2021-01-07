e9ba6effb9e341b24624893dea5ef0743eec6459
hemincong <hemincong@gmail.com>
2021-01-07 23:47:21 +0800

    remove cmake folder

--- cmake/modules/Findgtest.cmake
+++
@@ -1,4 +0,0 @@
-set(EXTERNAL_PATH ${PROJECT_SOURCE_DIR}/thirdparty)
-set(GTEST_DIR ${EXTERNAL_PATH}/googletest/googletest/)
-set(GTEST_INCLUDE_DIR ${GTEST_DIR}/include)
-set(GTEST_SRC_DIR ${GTEST_DIR}/src)
