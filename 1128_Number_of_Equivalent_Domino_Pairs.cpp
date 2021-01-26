dfc2439664452ff34d55e3a4f095291d9a145f64
hemc <hemc@szca.com>
2021-01-26 17:35:34 +0800

    + 1128. Number of Equivalent Domino Pairs

---
+++ 1128_Number_of_Equivalent_Domino_Pairs.cpp
@@ -0,0 +1,20 @@
+//
+// Created by mincong.he on 2021/1/26.
+//
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>> &dominoes) {
+        vector<int> num(100);
+        int ret = 0;
+        for (auto &it : dominoes) {
+            int val = it[0] < it[1] ? it[0] * 10 + it[1] : it[1] * 10 + it[0];
+            ret += num[val];
+            num[val]++;
+        }
+        return ret;
+    }
+};
