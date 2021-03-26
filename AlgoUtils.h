51d643dd38fe21a725ae2fc301b594f58b223000
hemc <hemc@szca.com>
2021-03-27 02:19:58 +0800

    remove unused code

--- AlgoUtils.h
+++ AlgoUtils.h
@@ -25,54 +25,6 @@
         std::cout << __FILE__ << ":" << __LINE__ << " ret: true expect: false" << std::endl;
 
 
-template<class Container, class Stream>
-Stream &printOneValueContainer
-        (Stream &outputstream, const Container &container) {
-    typename Container::const_iterator beg = container.begin();
-
-    outputstream << "[";
-
-    while (beg != container.end()) {
-        outputstream << " " << *beg++;
-    }
-
-    outputstream << " ]";
-
-    return outputstream;
-}
-
-template<class Type, class Container>
-const Container &container
-        (const std::stack<Type, Container> &stack) {
-    struct HackedStack : private std::stack<Type, Container> {
-        static const Container &container
-                (const std::stack<Type, Container> &stack) {
-            return stack.*&HackedStack::c;
-        }
-    };
-
-    return HackedStack::container(stack);
-}
-
-template<class Type, class Container>
-const Container &container
-        (const std::queue<Type, Container> &queue) {
-    struct HackedQueue : private std::queue<Type, Container> {
-        static const Container &container
-                (const std::queue<Type, Container> &queue) {
-            return queue.*&HackedQueue::c;
-        }
-    };
-
-    return HackedQueue::container(queue);
-}
-
-template<class Type, template<class c, class Container = std::deque<Type> > class Adapter, class Stream>
-Stream &operator<<
-        (Stream &outputstream, const Adapter<Type> &adapter) {
-    return printOneValueContainer(outputstream, container(adapter));
-}
-
 template<class T>
 void printArray(std::vector<T> ret) {
     for (const auto &r : ret) {
@@ -136,6 +88,7 @@ struct TreeNode {
     T val;
     TreeNode *left;
     TreeNode *right;
+
     TreeNode(T x) : val(x), left(NULL), right(NULL) {}
 };
 
@@ -149,6 +102,7 @@ struct ListNode {
 
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
 ListNode *build_list_node(const std::vector<int> &input) {
     if (input.empty()) return nullptr;
 
