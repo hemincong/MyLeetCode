fc718d0492bd9af50082860d94f5c4b3efc8e760
hemc <hemc@szca.com>
2021-03-31 23:28:21 +0800

    reformat code and more test case

--- 90_Subsets_II.cpp
+++ 90_Subsets_II.cpp
@@ -4,7 +4,7 @@
 
 #include <vector>
 #include <set>
-#include <iostream>
+#include "AlgoUtils.h"
 
 using namespace std;
 
@@ -15,35 +15,34 @@ public:
         std::vector<int> item;
         std::sort(nums.begin(), nums.end());
         result.push_back(item);
-        generate(0, nums, item, result);
+        generator(nums, 0, item, result);
         std::set<std::vector<int>> r_s(result.begin(), result.end());
         std::vector<std::vector<int>> r(r_s.begin(), r_s.end());
         return r;
     }
 
 private:
-    void generate(int i, std::vector<int> &nums, std::vector<int> &item, std::vector<std::vector<int>> &result) {
-        if (i >= nums.size()) return;
+    void generator(const vector<int> &nums, int index, vector<int> &item, vector<vector<int>> &ret) {
+        if (index >= nums.size()) return;
 
-        item.push_back(nums[i]);
-        result.push_back(item);
+        item.push_back(nums[index]);
+        ret.push_back(item);
 
-        generate(i + 1, nums, item, result);
+        generator(nums, index + 1, item, ret);
         item.pop_back();
-        generate(i + 1, nums, item, result);
+        generator(nums, index + 1, item, ret);
     }
 };
 
 int main(int argc, char **argv) {
-    std::vector<int> nums = {4, 4, 4, 1, 4};
-    Solution solution;
-    auto ret = solution.subsetsWithDup(nums);
-    for (const auto &i : ret) {
-        std::cout << "[";
-        for (const auto &j : i) {
-            std::cout << j;
-        }
-        std::cout << "]";
-    }
+    Solution s;
+    std::vector<int> test_case_1{4, 4, 4, 1, 4};
+    printMatrix(s.subsetsWithDup(test_case_1));
+
+    std::vector<int> test_case_2{1, 2, 2};
+    printMatrix(s.subsetsWithDup(test_case_2));
+
+    std::vector<int> test_case_3{0};
+    printMatrix(s.subsetsWithDup(test_case_3));
     return EXIT_SUCCESS;
 }
