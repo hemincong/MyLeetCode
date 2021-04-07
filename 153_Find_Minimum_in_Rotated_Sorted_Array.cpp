7f080ab2c5f4e89389aea343962a56bf4369f4bc
hemc <hemincong@163.com>
2021-04-08 00:56:11 +0800

    + new solution

--- 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -11,17 +11,31 @@ class Solution {
 public:
     int findMin(vector<int> &nums) {
         if (nums.empty()) return 0;
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (nums[mid] < nums[r]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return nums[l];
+    }
+};
+
+class Solution_2 {
+public:
+    int findMin(vector<int> &nums) {
+        if (nums.empty()) return 0;
 
         int min = nums[0];
-        bool rotated = false;
 
         for (int i = 0; i < nums.size() - 1; i++) {
             if (nums[i + 1] < nums[0]) {
-                if (rotated) return min;
-
-                rotated = true;
-
-                min = nums[i + 1];
+                return nums[i + 1];
             }
         }
 
@@ -35,9 +49,11 @@ int main(int argc, char **argv) {
     auto case_1 = vector<int>{3, 4, 5, 1, 2};
     auto case_2 = vector<int>{4, 5, 6, 7, 0, 1, 2};
     auto case_3 = vector<int>{11, 13, 15, 17};
+    auto case_4 = vector<int>{3, 1, 2};
     EXPECT_EQ(s.findMin(case_0), 0);
     EXPECT_EQ(s.findMin(case_1), 1);
     EXPECT_EQ(s.findMin(case_2), 0);
     EXPECT_EQ(s.findMin(case_3), 11);
+    EXPECT_EQ(s.findMin(case_4), 1);
     return EXIT_SUCCESS;
 }
