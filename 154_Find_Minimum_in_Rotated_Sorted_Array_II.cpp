9c163b3f99ac23abd099c654988e0a1f6601e592
hemc <hemincong@163.com>
2021-04-09 01:43:35 +0800

    + one more solution

--- 154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp
+++ 154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp
@@ -11,17 +11,34 @@ class Solution {
 public:
     int findMin(vector<int> &nums) {
         if (nums.empty()) return 0;
+        int l = 0;
+        int r = nums.size() - 1;
+        int mid = l + ((r - l) >> 1);
+        while (l < r) {
+            mid = l + ((r - l) >> 1);
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else if (nums[mid] > nums[l]) {
+                r = mid ;
+            } else {
+                r--;
+            }
+        }
+        return nums[l];
+    }
+};
+
+class Solution_2 {
+public:
+    int findMin(vector<int> &nums) {
+        if (nums.empty()) return 0;
 
         int min = nums[0];
-        bool rotated = false;
 
         for (int i = 0; i < nums.size() - 1; i++) {
             if (nums[i + 1] < nums[0]) {
-                if (rotated) return min;
-
-                rotated = true;
-
                 min = nums[i + 1];
+                break;
             }
         }
 
