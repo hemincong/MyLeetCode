00813e38eaaf46ad6d166bd6903a28209e1a8d9a
hemc <hemc@szca.com>
2021-05-03 01:31:01 +0800

    reformat code

--- 7_Reverse_Integer.cpp
+++ 7_Reverse_Integer.cpp
@@ -8,14 +8,14 @@ public:
         long i = 0;
         while (x != 0) {
             auto t = x % 10;
-            i = i*10 + t;
+            i = i * 10 + t;
             x /= 10;
         }
         return i > INT_MAX || i < INT_MIN ? (int) 0 : i;
     }
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     Solution s;
     EXPECT_EQ(s.reverse(123), 321)
     EXPECT_EQ(s.reverse(-123), -321)
