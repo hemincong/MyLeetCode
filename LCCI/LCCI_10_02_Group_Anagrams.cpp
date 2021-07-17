f655c79758be36933716074f9aaa5912bf7c4c3b
hemc <hemc@szca.com>
2021-07-18 02:33:33 +0800

    + 面试题 10.02. 变位词组

---
+++ LCCI/LCCI_10_02_Group_Anagrams.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <unordered_map>
+#include "AlgoUtils.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs) {
+        vector<vector<string>> ret;
+        unordered_map<string, vector<string>> m;
+        for (auto &s : strs) {
+            auto key = s;
+            sort(key.begin(), key.end());
+            m[key].emplace_back(s);
+        }
+        ret.reserve(m.size());
+        for (auto &p:m) {
+            ret.emplace_back(p.second);
+        }
+        return ret;
+    }
+};
+
+int main(int argc, char **argv) {
+    Solution s;
+    vector<string> test_case_1{"eat", "tea", "tan", "ate", "nat", "bat"};
+    printMatrix(s.groupAnagrams(test_case_1));
+
+    return EXIT_SUCCESS;
+}
