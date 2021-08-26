93bf3f4a6e62095e08b2a150b9dc3ca09beacab5
hemc <hemc@szca.com>
2021-08-26 11:05:20 +0800

    + 151. Best Time to Buy and Sell Stock

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include "AlgoUtils.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+        vector<int> dp(n, 0);
+        int min_prices = prices[0];
+        int ret = 0;
+        for (int i = 1; i < n; ++i) {
+            ret = max(ret, prices[i] - min_prices);
+            if (min_prices > prices[i])
+                min_prices = prices[i];
+        }
+        return ret;
+    }
+};
+
+int main(int argc, char **argv) {
+    Solution s;
+    vector<int> prices_1{7, 1, 5, 3, 6, 4};
+    EXPECT_EQ(s.maxProfit(prices_1), 5);
+    vector<int> prices_2{7, 6, 4, 3, 1};
+    EXPECT_EQ(s.maxProfit(prices_2), 0);
+    vector<int> prices_3{1, 2};
+    EXPECT_EQ(s.maxProfit(prices_3), 1);
+
+    return EXIT_SUCCESS;
+}
