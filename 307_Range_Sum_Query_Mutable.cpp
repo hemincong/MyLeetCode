3c29d11cf9a2f7392fc4ffcabe4fd536d3551994
hemc <hemc@szca.com>
2022-08-15 18:02:03 +0800

    ^ 307. Range Sum Query - Mutable

--- 307_Range_Sum_Query_Mutable.cpp
+++ 307_Range_Sum_Query_Mutable.cpp
@@ -4,78 +4,72 @@
 
 #include "AlgoUtils.h"
 #include <vector>
+#include <string>
 
 using namespace std;
-
-void build_segment_tree(std::vector<int> &value,
-                        std::vector<int> &nums,
-                        int pos, int left, int right) {
-    if (left == right) {
-        value[pos] = nums[left];
-        return;
-    }
-    int mid = (left + right) / 2;
-    build_segment_tree(value, nums, pos * 2 + 1, left, mid);
-    build_segment_tree(value, nums, pos * 2 + 2, mid + 1, right);
-    value[pos] = value[pos * 2 + 1] + value[pos * 2 + 2];
-}
-
-int sum_range_segment_tree(std::vector<int> &value,
-                           int pos, int left, int right,
-                           int qleft, int qright) {
-    if (qleft > right || qright < left) {
-        return 0;
-    }
-    if (qleft <= left && qright >= right) {
-        return value[pos];
-    }
-    int mid = (left + right) / 2;
-    return sum_range_segment_tree(value, pos * 2 + 1,
-                                  left, mid, qleft, qright) +
-           sum_range_segment_tree(value, pos * 2 + 2, mid + 1,
-                                  right, qleft, qright);
-}
-
-void update_segment_tree(std::vector<int> &value,
-                         int pos, int left, int right,
-                         int index, int new_value) {
-    if (left == right && left == index) {
-        value[pos] = new_value;
-        return;
-    }
-    int mid = (left + right) / 2;
-    if (index <= mid) {
-        update_segment_tree(value, pos * 2 + 1,
-                            left, mid, index, new_value);
-    } else {
-        update_segment_tree(value, pos * 2 + 2, mid + 1,
-                            right, index, new_value);
-    }
-    value[pos] = value[pos * 2 + 1] + value[pos * 2 + 2];
-}
 
 
 class NumArray {
 public:
     NumArray(vector<int> &nums) :
-            _value(nums.size() * sizeof(int), 0),
-            _right_end(nums.size() - 1) {
-        if (nums.empty()) return;
-
-        build_segment_tree(_value, nums, 0, 0, _right_end);
+            _segment_tree(nums.size() * sizeof(int), 0),
+            _n(nums.size()) {
+        _build(0, 0, _n - 1, nums);
     }
 
     void update(int i, int val) {
-        update_segment_tree(_value, 0, 0, _right_end, i, val);
+        _change(i, val, 0, 0, _n - 1);
     }
 
-    int sumRange(int i, int j) {
-        return sum_range_segment_tree(_value, 0, 0, _right_end, i, j);
+    int sumRange(int left, int right) {
+        return _range(left, right, 0, 0, _n - 1);
     }
 
 private:
-    std::vector<int> _value;
-    int _right_end;
+    void _build(int node, int s, int e, vector<int> &nums) {
+        if (s == e) {
+            _segment_tree[node] = nums[s];
+            return;
+        }
+        int m = s + ((e - s) / 2) ;
+        _build(node * 2 + 1, s, m, nums);
+        _build(node * 2 + 2, m + 1, e, nums);
+        _segment_tree[node] = _segment_tree[node * 2 + 1] + _segment_tree[node * 2 + 2];
+    }
+
+    void _change(int index, int val, int node, int s, int e) {
+        if (s == e) {
+            _segment_tree[node] = val;
+            return;
+        }
+
+        int m = s + ((e - s) / 2) ;
+        if (index <= m) {
+            _change(index, val, node * 2 + 1, s, m);
+        } else {
+            _change(index, val, node * 2 + 2, m + 1, e);
+        }
+        _segment_tree[node] = _segment_tree[node * 2 + 1] + _segment_tree[node * 2 + 2];
+    }
+
+    int _range(int left, int right, int node, int s, int e) {
+        if (left == s && right == e) {
+            return _segment_tree[node];
+        }
+
+        int m = s + ((e - s) / 2) ;
+        if (right <= m) {
+            return _range(left, right, node * 2 + 1, s, m);
+        } else if (left > m) {
+            return _range(left, right, node * 2 + 2, m + 1, e);
+        } else {
+            return _range(left, m, node * 2 + 1, s, m) + _range(m+ 1, right, node * 2 + 2, m + 1, e);
+        }
+    }
+
+
+    std::vector<int> _segment_tree;
+    int _n;
 };
 
 /**
