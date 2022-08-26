5821ea80344042ccc11d9a5e347d1d5e0e13a2a8
hemc <hemc@szca.com>
2022-08-26 16:29:46 +0800

    + 658. Find K Closest Elements

---
+++ 658_Find_K_Closest_Elements.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include "AlgoUtils.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int> &arr, int k, int x) {
+        int l = 0;
+        int r = arr.size() - k;
+        while (l < r) {
+            auto m = l + (r - l) / 2;
+            if (x - arr[m] > arr[m + k] - x) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return {arr.begin() + l, arr.begin() + l + k};
+    }
+};
