4f2f74dc48d719ee2ffe28fdd380fd61694e12eb
mincong <hemincong@gmail.com>
2024-08-06 11:35:57 +0800

    + 234 回文链表

---
+++ 234_palindrome_linked_list.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func isPalindrome(_ head: ListNode?) -> Bool {
+        var array = [Int]()
+
+            var p = head
+            while p != nil {
+                array.append(p!.val)
+                    p = p!.next
+            }
+
+        var l = 0, r = array.count - 1;
+        while l < r {
+            if array[l] != array[r] {
+                return false
+            }
+            l += 1
+                r -= 1
+        }
+
+        return true
+
+    }
+}
